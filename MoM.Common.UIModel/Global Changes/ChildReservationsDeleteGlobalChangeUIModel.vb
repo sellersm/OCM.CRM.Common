a1395c86c44c490093e18bf9bcff545100ca8163
Mark Sellers <mark@masenterprisesinc.com;msellers>
2018-09-19 15:02:57 -0600

    New global changes requested.

---
+++ MoM.Common.UIModel/Global Changes/ChildReservationsDeleteGlobalChangeUIModel.vb
@@ -0,0 +1,7 @@
+Public Class ChildReservationsDeleteGlobalChangeUIModel
+
+    Private Sub ChildReservationsDeleteGlobalChangeUIModel_Loaded(ByVal sender As Object, ByVal e As Blackbaud.AppFx.UIModeling.Core.LoadedEventArgs) Handles Me.Loaded
+        Me.IDSETREGISTERID.Visible = True
+    End Sub
+
+End Class
